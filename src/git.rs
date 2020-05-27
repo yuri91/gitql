40c4afbb37852c878415460df1613509ecc7521d
Yuri Iozzelli <y.iozzelli@gmail.com>
2020-05-27 18:36:50 +0200

    Fitst commit. WIP

---
+++ src/git.rs
@@ -0,0 +1,95 @@
+use git2::{Repository, Signature};
+use log::info;
+use serde_derive::Deserialize;
+use slug::slugify;
+
+pub use git2::Error;
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub fn get_repo(path: &str) -> Result<Repository> {
+    Repository::open_bare(path).or_else(|_| {
+        info!("Creating bare repo at {}", path);
+        Repository::init_bare(path)
+    })
+}
+
+pub fn get_file(path: &str, repo: &Repository) -> Result<String> {
+    let obj = repo.revparse_single(&format!("master:{}", path))?;
+    let blob = obj.peel_to_blob()?;
+    let content = std::str::from_utf8(blob.content()).expect("not utf8");
+    Ok(content.to_owned())
+}
+pub fn list_files(path: &str, repo: &Repository) -> Result<Vec<Metadata>> {
+    let obj = repo.revparse_single(&format!("master:meta{}", path))?;
+    let tree = obj.peel_to_tree()?;
+    Ok(tree
+        .iter()
+        .map(|e| {
+            e.to_object(&repo).and_then(|o| o.peel_to_blob()).map(|b| {
+                serde_json::from_str(std::str::from_utf8(b.content()).expect("not utf8"))
+                    .expect("not json")
+            })
+        })
+        .filter_map(Result::ok)
+        .collect())
+}
+
+#[derive(Deserialize, Debug)]
+pub struct CommitInfo {
+    pub content: String,
+    pub title: String,
+}
+fn transpose<T, E>(o: Option<std::result::Result<T, E>>) -> std::result::Result<Option<T>, E> {
+    match o {
+        Some(Ok(x)) => Ok(Some(x)),
+        Some(Err(e)) => Err(e),
+        None => Ok(None),
+    }
+}
+//pub fn page_committer(author: String, info: CommitInfo, repo_path: &str) -> Result<String> {
+//    let link = slugify(&info.title);
+//    let repo = get_repo(&repo_path)?;
+//
+//    let obj = repo.revparse_single("master:")?;
+//    let tree = obj.peel_to_tree()?;
+//
+//    let mut treebuilder = repo.treebuilder(Some(&tree))?;
+//    let blob = repo.blob(info.content.as_bytes())?;
+//    treebuilder.insert(&format!("{}.md", link), blob, 0o100_644)?;
+//
+//    let meta = super::page::Metadata {
+//        title: info.title,
+//        link,
+//    };
+//    let blob = repo.blob(
+//        serde_json::to_string(&meta)
+//            .expect("cannot serialize")
+//            .as_bytes(),
+//    )?;
+//    let mut metatreebuilder = repo.treebuilder(
+//        transpose(
+//            tree.get_name("meta")
+//                .map(|t| t.to_object(&repo).and_then(|t| t.peel_to_tree())),
+//        )?
+//        .as_ref(),
+//    )?;
+//    metatreebuilder.insert(&format!("{}.json", meta.link), blob, 0o100_644)?;
+//    let oid = metatreebuilder.write()?;
+//    treebuilder.insert("meta", oid, 0o040_000)?;
+//
+//    let oid = treebuilder.write()?;
+//    let newtree = repo.find_tree(oid)?;
+//
+//    let sig = Signature::now(&author, &format!("{}@peori.space", author))?;
+//    let branch = repo.find_branch("master", git2::BranchType::Local)?;
+//    repo.commit(
+//        branch.get().name(),
+//        &sig,
+//        &sig,
+//        "Edited from web interface",
+//        &newtree,
+//        &[&branch.get().peel_to_commit()?],
+//    )?;
+//
+//    Ok(meta.link)
+//}
---
+++ src/main.rs
@@ -0,0 +1,87 @@
+use async_graphql::http::playground_source;
+use async_graphql::{Context, EmptyMutation, EmptySubscription, Schema};
+use async_std::task;
+use std::env;
+use tide::{
+    http::{headers, mime},
+    Request, Response, StatusCode,
+};
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+type Repo = async_std::sync::Mutex<git2::Repository>;
+
+mod git;
+
+struct MyToken(String);
+
+struct QueryRoot;
+
+#[async_graphql::Object]
+impl QueryRoot {
+    async fn page<'a>(&self, _ctx: &'a Context<'_>, path: &str) -> Page {
+        Page { path }
+    }
+}
+
+struct Page {
+    path: String,
+}
+
+#[async_graphql::Object]
+impl Page {
+    async fn content<'a>(&self, ctx: &'a Context<'_>) -> String {
+        let repo = ctx.data::<Repo>().lock().await;
+        git::get_file(&self.path, &repo).expect("error page")
+    }
+}
+
+struct AppState {
+    schema: Schema<QueryRoot, EmptyMutation, EmptySubscription>,
+    repo: git2::Repository,
+}
+
+fn main() -> Result<()> {
+    pretty_env_logger::init();
+    task::block_on(run())
+}
+
+async fn run() -> Result<()> {
+    let listen_addr = env::var("LISTEN_ADDR").unwrap_or_else(|_| "localhost:8000".to_owned());
+
+    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription).finish();
+
+    println!("Playground: http://{}", listen_addr);
+
+    let repo = git::get_repo("repo");
+
+    let app_state = AppState { schema, repo };
+    let mut app = tide::with_state(app_state);
+
+    async fn graphql(req: Request<AppState>) -> tide::Result<Response> {
+        let schema = req.state().schema.clone();
+        let token = &req
+            .header(&"token".parse().unwrap())
+            .and_then(|values| values.first().map(|value| value.to_string()));
+
+        async_graphql_tide::graphql(req, schema, |mut query_builder| {
+            if let Some(token) = token {
+                query_builder = query_builder.data(Repo::new(git::get_repo("repo").expect("no repo")));
+            }
+            query_builder
+        })
+        .await
+    }
+
+    app.at("/graphql").post(graphql).get(graphql);
+    app.at("/").get(|_| async move {
+        let resp = Response::new(StatusCode::Ok)
+            .body_string(playground_source("/graphql", None))
+            .set_header(headers::CONTENT_TYPE, mime::HTML.to_string());
+
+        Ok(resp)
+    });
+
+    app.listen(listen_addr).await?;
+
+    Ok(())
+}
